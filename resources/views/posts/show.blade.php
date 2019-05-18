a2465f343279c6fb75f911b5bd18c218dc8eeedb
Thanapong Yingyong <thanapongtle@gmail.com>
2019-05-19 01:21:25 +0700

    🛠 re-design: view, comment

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,71 +3,76 @@
 @section('content')
     <div class="container">
         <div class="row">
-
-            <div class="col-md-12">
-                <div class="card">
+            <div class="col-md-12" style="box-shadow: 0 2px 4px 0 rgba(0,0,0,0.12);">
+                {{-- <div class="card">
                     <div class="card-header">{{ $post->name }}
                     <hr>
                     โพสโดย {{$post->user->name}} ({{$post->user->email}})
-                    </div>
+                    </div> --}}
                     <div class="card-body">
-
-                        <a href="{{ url('/posts') }}" title="Back"><button class="btn btn-warning btn-sm"><i class="fa fa-arrow-left" aria-hidden="true"></i> Back</button></a>
+                        {{-- <a href="{{ url('/posts') }}" title="Back"><button class="btn btn-warning btn-sm"><i class="fa fa-arrow-left" aria-hidden="true"></i> Back</button></a> --}}
                         @if($post->check)
+                            <a href="{{ url('/posts/' . $post->id . '/edit') }}" title="Edit Post"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
 
-                        <a href="{{ url('/posts/' . $post->id . '/edit') }}" title="Edit Post"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
+                            <form method="POST" action="{{ url('posts' . '/' . $post->id) }}" accept-charset="UTF-8" style="display:inline">
+                                {{ method_field('DELETE') }}
+                                {{ csrf_field() }}
+                                <button type="submit" class="btn btn-danger btn-sm" title="Delete Post" onclick="return confirm(&quot;Confirm delete?&quot;)"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</button>
+                            </form>
 
-                        <form method="POST" action="{{ url('posts' . '/' . $post->id) }}" accept-charset="UTF-8" style="display:inline">
-                            {{ method_field('DELETE') }}
-                            {{ csrf_field() }}
-                            <button type="submit" class="btn btn-danger btn-sm" title="Delete Post" onclick="return confirm(&quot;Confirm delete?&quot;)"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</button>
-                        </form>
-
-                        <form method="POST" action="{{ url('post/update_status' . '/' . $post->id) }}" accept-charset="UTF-8" style="display:inline">
-                            {{ csrf_field() }}
-                            <button type="submit" class="btn btn-success btn-sm" title="Delete Post" onclick="return confirm(&quot;ทำการยืนยัน โพสของคุณจะถูกปิดการแสดงความคิดเห็นและเปลี่ยนสถานะ?&quot;)"><i class="fa fa-correct" aria-hidden="true"></i> ฉันได้ทำการแลกเปลี่ยนเสร็จสิ้น</button>
-                        </form>
-
+                            <form method="POST" action="{{ url('post/update_status' . '/' . $post->id) }}" accept-charset="UTF-8" style="display:inline">
+                                {{ csrf_field() }}
+                                <button type="submit" class="btn btn-success btn-sm" title="Delete Post" onclick="return confirm(&quot;ทำการยืนยัน โพสของคุณจะถูกปิดการแสดงความคิดเห็นและเปลี่ยนสถานะ?&quot;)"><i class="fa fa-correct" aria-hidden="true"></i> ฉันได้ทำการแลกเปลี่ยนเสร็จสิ้น</button>
+                            </form>
                         @endif
-                        <br/>
-                        <br/>
 
-                        <div class="table-responsive">
-                        <img src="{{$post->img_url}}" height="300" width="100%">
-                            <table class="table">
-                                <tbody>
-                                    <tr><th> Name </th><td> {{ $post->name }} </td></tr><tr><th> Description </th><td> {{ $post->description }} </td></tr>
-                                </tbody>
-                            </table>
+                        <div class="form-group">
+                            <h5 class="card-title" style="font-weight: 800;">{{ $post->name }}</h5>
+                        </div>
+
+                        <div class="form-group">
+                            <img src="{{$post->img_url}}" height="300" width="100%">
+                        </div>
+
+                        <div class="form-group">
+                            <h5 class="card-title" style="font-weight: 800;">รายละเอียด</h5>
+                            <p class="card-text">{{ $post->description }}</p>
                         </div>
 
                     </div>
-
-                    <div class="p-4">
-                            <h3>Comments</h3>
-                            @if (Auth::check() && !$post->status)
-                            {{ Form::open(['route' => ['comments.store'], 'method' => 'POST']) }}
-                            <p>{{ Form::textarea('body', old('body')) }}</p>
-                            {{ Form::hidden('post_id', $post->id) }}
-                            <p>{{ Form::submit('Send') }}</p>
-                            {{ Form::close() }}
-                            @else
-                            <h3 class="p-4">
-                            ความคิดเห็นถูกปิดการใช้งาน เนื่องจาก โพสนี้ได้ทำการแลกเปลี่ยนเรียบร้อยแล้ว
-                            </h3>
-                            @endif
-                            @forelse ($post->comments as $comment)
-                            <p>{{ $comment->user->name }} {{$comment->created_at}}</p>
-                            <p>{{ $comment->body }}</p>
-                            <hr>
-                            @empty
-                            <p>This post has no comments</p>
-                            @endforelse
+            </div>
+                    <div class="col-md-12" style="margin-top: 28px; box-shadow: 0 2px 4px 0 rgba(0,0,0,0.12); padding-bottom: 22px;">
+                            <div class="p-4">
+                                    <h3 style="font-size: 16px; font-weight: 800;">แสดงความคิดเห็น</h3>
+                                    @if (Auth::check() && !$post->status)
+                                    {{ Form::open(['route' => ['comments.store'], 'method' => 'POST']) }}
+                                    <p>{{ Form::textarea('body', old('body')) }}</p>
+                                    {{ Form::hidden('post_id', $post->id) }}
+                                    <p class="btn-submit-comment">{{ Form::submit('แสดงความคิดเห็น') }}</p>
+                                    {{ Form::close() }}
+                                    @else
+                                    <h3 class="p-4">
+                                    ความคิดเห็นถูกปิดการใช้งาน เนื่องจาก โพสนี้ได้ทำการแลกเปลี่ยนเรียบร้อยแล้ว
+                                    </h3>
+                                    @endif
+                                    
+                            </div>
                     </div>
 
-
-                </div>
-            </div>
+                    <div class="col-md-12" style="margin-top: 28px; box-shadow: 0 2px 4px 0 rgba(0,0,0,0.12); padding-bottom: 22px;">
+                            <div class="p-4">
+                                    @forelse ($post->comments as $comment)
+                                    <h3 style="font-size: 16px; font-weight: 800;">ความคิดเห็น</h3>
+                                    <p>{{ $comment->body }}</p>
+                                    <p>Post by {{ $comment->user->name }}</p>
+                                    <hr>
+                                    @empty
+                                    <p></p>
+                                    @endforelse
+                            </div>
+                    </div>
+    
+        </div>
         </div>
     </div>
 
